e3478af482eedc384326db39989d36a2e2daa2c3
Adil Keku Gazder <69271111+ag826@users.noreply.github.com>
2024-08-18 18:47:29 -0400

    Add files via upload

---
+++ LandingPage.jsx
@@ -0,0 +1,332 @@
+import React from "react";
+import { Frame } from "../../components/Frame";
+import { Rectangle } from "../../components/Rectangle";
+import "./style.css";
+
+export const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="overlap-wrapper">
+        <div className="overlap-2">
+          <div className="overlap-3">
+            <Rectangle className="rectangle-4" />
+            <div className="rectangle-2" />
+            <div className="rectangle-3" />
+            <div className="ellipse" />
+            <img
+              className="IM-a-DATA-SCIENTIST"
+              alt="Im a DATA SCIENTIST"
+              src="/img/i-m-a-data-scientist-decision-consultant.png"
+            />
+            <div className="text-wrapper-2">HI, I’M ADIL</div>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-4">
+                <p className="p">Including internships and full time positions</p>
+                <div className="text-wrapper-3">3+ years</div>
+                <div className="text-wrapper-4">Experiece of over</div>
+              </div>
+            </div>
+            <div className="div-wrapper">
+              <div className="overlap-5">
+                <div className="text-wrapper-5">Exposure to</div>
+                <p className="text-wrapper-6">Spanning Healthcare, Logistics and Aviation</p>
+                <div className="text-wrapper-7">3 industries</div>
+              </div>
+            </div>
+            <div className="group-2">
+              <div className="overlap-6">
+                <div className="text-wrapper-8">Proficient in</div>
+                <p className="text-wrapper-9">For data cleaning, modeling and visualization</p>
+                <div className="text-wrapper-10">10+ tools</div>
+              </div>
+            </div>
+            <img className="line-2" alt="Line" src="/img/line-3.svg" />
+            <img className="line-3" alt="Line" src="/img/line-3.svg" />
+            <img className="IMG" alt="Img" src="/img/img-20221003-wa00131-1.png" />
+            <div className="group-3">
+              <div className="overlap-7">
+                <div className="overlap-8">
+                  <div className="text-wrapper-11">Just Deliveries LLC</div>
+                  <div className="text-wrapper-12">Growth and Strategy</div>
+                  <p className="text-wrapper-13">
+                    Led the DTC pilot project, optimizing and digitizing the delivery network model by developing
+                    performance dashboards, which reduced human error and Return to Vendor (RTV) orders by 18%. Secured
+                    seed funding and multiple grants from prominent sector-agnostic fund pools, earning recognition as
+                    one of the Top 100 Startups in Maharashtra (MSINS, 2021). Directed the market research team to
+                    explore diversification opportunities in Retail and HoReCa sectors.
+                  </p>
+                </div>
+                <img className="image" alt="Image" src="/img/image-8.png" />
+              </div>
+              <div className="overlap-9">
+                <div className="ellipse-2" />
+                <div className="element-mumbai-MH">
+                  2021
+                  <br />
+                  Mumbai, MH
+                </div>
+              </div>
+              <div className="group-4">
+                <div className="overlap-10">
+                  <div className="rectangle-5" />
+                  <p className="text-wrapper-14">+ Go To Market Strategy</p>
+                  <div className="group-5">
+                    <div className="overlap-10">
+                      <div className="rectangle-6" />
+                      <div className="text-wrapper-15">+ Venture Financing</div>
+                      <div className="group-6">
+                        <div className="overlap-group-2">
+                          <div className="rectangle-7" />
+                          <div className="text-wrapper-16">+ Automation</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="group-7">
+                <div className="overlap-10">
+                  <div className="rectangle-5" />
+                  <p className="text-wrapper-14">+ Go To Market Strategy</p>
+                  <div className="group-5">
+                    <div className="overlap-10">
+                      <div className="rectangle-6" />
+                      <div className="text-wrapper-15">+ Venture Financing</div>
+                      <div className="group-6">
+                        <div className="overlap-group-2">
+                          <div className="rectangle-7" />
+                          <div className="text-wrapper-16">+ Automation</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="group-8">
+              <div className="overlap-11">
+                <div className="text-wrapper-17">Shiv Nadar University</div>
+                <div className="ellipse-3" />
+                <div className="element-delhi-NCR">
+                  2018
+                  <br />
+                  Delhi NCR
+                </div>
+                <p className="text-wrapper-18">
+                  Bachelor of Technology (B.Tech.) in Mechanical Engineering, Class of 2022
+                </p>
+                <p className="text-wrapper-19">
+                  CGPA: 8.33; Awarded a 70% scholarship for consistent academic excellence. Specialized in Material
+                  Science and Engineering with a minor in Computer Science and Engineering. Elected as Secretary of
+                  External Affairs in the Student Council, where I increased external participation by 83% and signed
+                  three MOUs to promote cultural growth. Co-founded and led the Martial Arts Society as the primary
+                  instructor, conducting over 100 sessions and training the cohort for national competitions, earning
+                  both collegiate and national recognition.
+                </p>
+                <img className="image-2" alt="Image" src="/img/image-4.png" />
+              </div>
+            </div>
+            <img className="arrow" alt="Arrow" src="/img/arrow-2.svg" />
+            <div className="group-9">
+              <div className="overlap-12">
+                <div className="group-10">
+                  <div className="overlap-13">
+                    <div className="group-11">
+                      <div className="overlap-14">
+                        <img className="image-3" alt="Image" src="/img/image-1.png" />
+                        <div className="overlap-group-3">
+                          <div className="text-wrapper-20">Duke University</div>
+                          <p className="text-wrapper-21">
+                            Master of Interdisciplinary Data Science (MIDS) program, Class of 2026
+                          </p>
+                          <p className="text-wrapper-22">
+                            Key coursework includes Data Engineering Systems, Data Logic, Visualization and
+                            Storytelling, Natural Language Processing, Unifying Data Science, and Intro to Algorithmic
+                            Trading.
+                          </p>
+                        </div>
+                      </div>
+                      <div className="ellipse-4" />
+                    </div>
+                    <div className="element-durham-NC">
+                      2024
+                      <br />
+                      Durham, NC
+                    </div>
+                  </div>
+                </div>
+                <div className="group-12">
+                  <div className="overlap-15">
+                    <div className="group-13">
+                      <div className="overlap-group-4">
+                        <div className="rectangle-8" />
+                        <div className="text-wrapper-23">+ Machine Learning</div>
+                      </div>
+                    </div>
+                    <div className="group-14">
+                      <div className="overlap-10">
+                        <div className="rectangle-8" />
+                        <div className="text-wrapper-24">+ Data Engineering</div>
+                        <div className="group-15">
+                          <div className="overlap-group-4">
+                            <div className="rectangle-9" />
+                            <div className="text-wrapper-23">+ Data Visualization</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="group-16">
+              <a href="https://duke.edu/" rel="noopener noreferrer" target="_blank">
+                <p className="graduating-from-duke">
+                  <span className="span">
+                    Graduating from <br />
+                  </span>
+                  <span className="text-wrapper-25">Duke University</span>
+                  <span className="span">
+                    {" "}
+                    <br />
+                    (Class of ‘26)
+                  </span>
+                </p>
+              </a>
+              <a
+                href="https://www.google.com/search?q=durham+NC&amp;rlz=1C1VDKB_enIN1079IN1082&amp;oq=durham+NC&amp;gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg9MgYIAhBFGD0yBggDEEUYQdIBCDIxNThqMGo3qAIAsAIA&amp;sourceid=chrome&amp;ie=UTF-8"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <p className="based-out-of-durham">
+                  <a
+                    href="https://www.google.com/search?q=durham+NC&amp;rlz=1C1VDKB_enIN1079IN1082&amp;oq=durham+NC&amp;gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg9MgYIAhBFGD0yBggDEEUYQdIBCDIxNThqMGo3qAIAsAIA&amp;sourceid=chrome&amp;ie=UTF-8"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <span className="span">
+                      Based out of <br />
+                    </span>
+                  </a>
+                  <a
+                    href="https://www.google.com/search?q=durham+NC&amp;rlz=1C1VDKB_enIN1079IN1082&amp;oq=durham+NC&amp;gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg9MgYIAhBFGD0yBggDEEUYQdIBCDIxNThqMGo3qAIAsAIA&amp;sourceid=chrome&amp;ie=UTF-8"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <span className="text-wrapper-25">Durham, North Carolina</span>
+                  </a>
+                </p>
+              </a>
+              <div className="overlap-16">
+                <a href="https://www.ice.gov/sevis/practical-training" rel="noopener noreferrer" target="_blank">
+                  <p className="CPT-OPT-STEM-OPT">
+                    <a href="https://www.ice.gov/sevis/practical-training" rel="noopener noreferrer" target="_blank">
+                      <span className="text-wrapper-25">CPT / OPT / STEM OPT </span>
+                    </a>
+                    <a href="https://www.ice.gov/sevis/practical-training" rel="noopener noreferrer" target="_blank">
+                      <span className="text-wrapper-26">
+                        {" "}
+                        <br />
+                      </span>
+                    </a>
+                    <a href="https://www.ice.gov/sevis/practical-training" rel="noopener noreferrer" target="_blank">
+                      <span className="span">Work Authorization</span>
+                    </a>
+                  </p>
+                </a>
+                <p className="looking-for-summer">
+                  <span className="span">Looking for </span>
+                  <span className="text-wrapper-27">
+                    Summer Internship 2025
+                    <br />{" "}
+                  </span>
+                  <span className="span">and </span>
+                  <span className="text-wrapper-27">Full Time 2026</span>
+                  <span className="span"> offers</span>
+                </p>
+              </div>
+            </div>
+            <p className="text-wrapper-28">
+              An experienced consultant with a strong interdisciplinary background, specializing in leveraging
+              data-driven strategies to optimize trading algorithms and financial models. Adept at driving decisions in
+              fast-paced financial environments, with a deep understanding of market dynamics, statistical modeling, and
+              trading algorithms. Outside work, a two time national level gold medalist in Taekwondo and a black belt
+              holder in Karate. Enjoys adventure sports, podcasts and traveling. Hopes to host his own talk show
+              someday.
+            </p>
+            <p className="WHO-IS-ADIL-GAZDER">
+              <span className="text-wrapper-29">WHO IS </span>
+              <span className="text-wrapper-30">ADIL GAZDER?</span>
+            </p>
+            <div className="frame-2" />
+            <div className="VIEW-MY-PROJECTS"> VIEW MY PROJECTS</div>
+            <div className="group-17">
+              <div className="overlap-17">
+                <div className="text-wrapper-31">LET’S CONNECT!</div>
+              </div>
+            </div>
+            <div className="group-18">
+              <div className="group-19">
+                <div className="overlap-group-5">
+                  <p className="text-wrapper-32">
+                    Analyzed and forecasted treatment patterns for over 19,000 oncologists in the US lymphoma market,
+                    applying predictive modeling to anticipate market trends and drive strategic decisions in the lung
+                    cancer sector. Developed and managed analytics pipelines and dashboards for 279,000+ cancer
+                    patients, utilizing advanced clustering techniques to segment doctors and tailor marketing
+                    strategies accordingly. Conducted in-depth market research, analyzing patient charts and responses
+                    to provide actionable insights into the leukemia treatment landscape and preferences.
+                  </p>
+                  <div className="text-wrapper-33">ZS Associates</div>
+                  <p className="text-wrapper-34">Decision Analytics - Senior Associate</p>
+                  <div className="element-gurgaon-HR">
+                    2022
+                    <br />
+                    Gurgaon, HR
+                  </div>
+                  <img className="image-4" alt="Image" src="/img/image-2.png" />
+                  <div className="ellipse-5" />
+                </div>
+              </div>
+              <div className="group-20">
+                <div className="overlap-18">
+                  <div className="group-21">
+                    <div className="overlap-group-6">
+                      <div className="rectangle-10" />
+                      <div className="text-wrapper-35">+ Predictive Analytics</div>
+                    </div>
+                  </div>
+                  <div className="group-22">
+                    <div className="overlap-19">
+                      <div className="rectangle-11" />
+                      <div className="text-wrapper-35">+ Diagnostic Analytics</div>
+                      <div className="group-wrapper">
+                        <div className="group-23">
+                          <div className="overlap-group-6">
+                            <div className="rectangle-12" />
+                            <div className="text-wrapper-35">+ Client Management</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <img className="line-4" alt="Line" src="/img/line-15.svg" />
+            <img className="line-5" alt="Line" src="/img/line-15.svg" />
+            <img className="line-6" alt="Line" src="/img/line-16.svg" />
+            <img className="line-7" alt="Line" src="/img/line-16.svg" />
+            <div className="group-24">
+              <div className="overlap-20">
+                <div className="rectangle-13" />
+                <Frame className="frame-6" />
+                <div className="text-wrapper-36">Adil Gazder</div>
+              </div>
+            </div>
+          </div>
+          <div className="rectangle-14" />
+        </div>
+      </div>
+    </div>
+  );
+};
